c48e69118aa17f712d5b95b603bdf048f8ab9097
Athabhadrika <athabhadrika11@gmail.com>
2026-02-24 12:35:38 +0800

    Initial commit: Obsidian Core Bot

---
+++ database/ControlApps.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8757372091:AAFr75i1qYevsALenbUaYvswB6IbioMLPmo", 
+  owner: "7664962537", 
+  port: "9362", // Ini Wajib Jangan Diubah
+  ipvps: "https://host.astavvip-tech.xyz" // Jangan Diubah Nanti Eror!!
+};
